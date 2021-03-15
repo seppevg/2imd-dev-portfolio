b16f3d01568d48e0add9364c4d1743f0a26826bf
Seppe Vangeel <seppe.vg@live.be>
2021-03-15 21:50:27 +0100

    Added showrandomcoffee function

--- Lab 4 - API/app.js
+++ Lab 4 - API/app.js
@@ -59,14 +59,12 @@ class App {
     chooseAdd() {
         let temperature = localStorage.getItem("temperature");
         temperature = JSON.parse(temperature);
-        this.showRandomCocktail();
-        /*
         if (temperature.temp > 15) {
             this.showRandomCocktail();
         }
         else {
             this.showRandomCoffee();
-        }*/
+        }
     }
 
     showRandomCocktail() {
@@ -84,6 +82,28 @@ class App {
                 let img_cocktail = json.drinks[0].strDrinkThumb;
                 //console.log(json.drinks[0].strDrinkThumb);
                 this.ad.style.background = `url(${img_cocktail})`;
+                this.ad.style.backgroundRepeat = "no-repeat";
+                this.ad.style.backgroundSize = "300px 600px";
+            })
+    }
+
+    showRandomCoffee() {
+        let url_coffeeAPI = `https://cors-anywhere.herokuapp.com/https://coffee.alexflipnote.dev/random.json`;
+        fetch(url_coffeeAPI)
+            .then((response) => {
+                console.log(response);
+                return response.json();
+            })
+            .then((json) => {
+                console.log(json);
+                this.weather.innerHTML = "Brr, het is wat frisjes! Even opwarmen? Neem dan een warme";
+                this.drink.innerHTML = "koffie van't huis";
+                //console.log(json.drinks[0].strDrink);
+                let img_coffee = json.file;
+                /* Koffie API heeft niet veel goede foto's => deze kan je gebruiken als test afbeelding
+                let img = `https://s23514.pcdn.co/wp-content/uploads/2019/05/03A0B4D8-273E-408A-A964-9F3E6CC6CD21-768x1024.jpeg`; */
+                console.log(img_coffee);
+                this.ad.style.background = `url(${img_coffee})`;
                 this.ad.style.backgroundRepeat = "no-repeat";
                 this.ad.style.backgroundSize = "300px 600px";
             })
