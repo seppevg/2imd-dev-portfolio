f017048fbb52258bccfc66c6e4a3ec5a990fbc87
Seppe Vangeel <seppe.vg@live.be>
2021-03-22 14:40:25 +0100

    Installed nodemon & Added start to scripts

--- LAB 5 - NodeJS/index.js
+++ LAB 5 - NodeJS/index.js
@@ -1,1 +1,29 @@
-console.log("lets go");
+/*Testing Node - Chalk
+const chalk = require('chalk');
+console.log(chalk.blue('Hello world!'));
+*/
+
+//Getting Started
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+
+    switch (req.url) {
+        case "/signup":
+            res.end('This is the sign up page!');
+            break;
+
+        default:
+            res.end('Hello World');
+            break;
+    }
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
