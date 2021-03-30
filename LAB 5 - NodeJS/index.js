e3e57dde6ec854807c862bd498da851c8c50f162
Seppe Vangeel <seppe.vg@live.be>
2021-03-30 16:32:07 +0200

    fix: add get, post, put & delete to index.js

--- LAB 5 - NodeJS/index.js
+++ LAB 5 - NodeJS/index.js
@@ -1,11 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
-})
+});
+
+app.get('/api/v1/messages', (req, res) => {
+    res.send('Get message');
+});
+app.post('/api/v1/messages', (req, res) => {
+    res.send('Post message');
+});
+app.put('/api/v1/messages/:id', (req, res) => {
+    res.send('Put message ' + req.params.id);
+});
+app.delete('/api/v1/messages/:id', (req, res) => {
+    res.send('Delete message ' + req.params.id);
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
-})
+});
