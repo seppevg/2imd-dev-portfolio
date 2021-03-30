1e65464cc574f760da2f0d8e12e31160cd14c577
Seppe Vangeel <seppe.vg@live.be>
2021-03-30 17:50:33 +0200

    Fix: install pug and made simple homepage

--- LAB 5 - NodeJS/index.js
+++ LAB 5 - NodeJS/index.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const messagesRouter = require('./routers/api/v1/messages');
+const pug = require('pug');
+
+app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.render('index', {
+        "title": 'Homepage',
+        "header": 'Welcome!',
+        "message": 'This MessagesAPI is very cool!'
+    });
 });
 
 app.use('/api/v1/messages', messagesRouter);
