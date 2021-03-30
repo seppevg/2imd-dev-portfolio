ba1169ec4f34ee8bb32d5acfee03bdbcc38f29e9
Seppe Vangeel <seppe.vg@live.be>
2021-03-30 16:22:54 +0200

    fix: create nodejs app, install express & nodemon

--- LAB 5 - NodeJS/index.js
+++ LAB 5 - NodeJS/index.js
@@ -1,29 +1,11 @@
-/*Testing Node - Chalk
-const chalk = require('chalk');
-console.log(chalk.blue('Hello world!'));
-*/
+const express = require('express')
+const app = express()
+const port = 3000
 
-//Getting Started
-const http = require('http');
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-
-    switch (req.url) {
-        case "/signup":
-            res.end('This is the sign up page!');
-            break;
-
-        default:
-            res.end('Hello World');
-            break;
-    }
-});
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
